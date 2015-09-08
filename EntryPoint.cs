1575e0315b0986e24ad9bea73128326736759897
Shivali Chandra <shivali.chandra@students.olin.edu>
2015-09-08 13:51:54 -0400

    Commit - basic hello world addin to send message

---
+++ EntryPoint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldAddIn
+{
+    using System.AddIn;
+    using ApxLabs.Skylight.AddIn;
+
+    [AddIn("HelloWorldAddIn")]
+    public class EntryPoint : ISkylightAddIn
+    {
+        private ISkylightHost _host;
+        public void Initialize(ISkylightHost host)
+        {
+            Console.WriteLine("The Hello World AddIn has been initialized");
+            _host = host;
+            _host.Connected += HostConnectedHandler;
+            _host.PresenceReceived += _host_PresenceReceived;
+        }
+
+        private void _host_PresenceReceived(object sender, PresenceReceivedEventArgs e)
+        {
+            Console.WriteLine("Presence Received");
+            if (e.IsAvailable)
+            {
+                _host.SendMessage(e.UserAccount, "Test", "Hello");
+                Console.WriteLine("User is online");
+            }
+            else
+            {
+                Console.WriteLine("There are no users online right now.");
+            }
+        }
+
+        private void HostConnectedHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
